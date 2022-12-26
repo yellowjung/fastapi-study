f4ddff77c1ad7352d4d17d181b1357c4cc9503b9
unknown <1328436@HIMART.INFRA>
2022-12-26 15:12:33 +0900

    Order matters

---
+++ second.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+    
+@app.get("/user/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+    
+@app.get("/user/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
